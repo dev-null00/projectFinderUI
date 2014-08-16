ecee62e56cb833c8ee0fec4de148d2ef3c306741
Solomon Lasluisa <raulslasluisa@gmail.com>
2014-08-16 19:55:49 -0400

    move to dev branch

---
+++ test/karma.conf.js
@@ -0,0 +1,40 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : '../',
+
+    files : [
+      'app/bower_components/angular/angular.js',
+      'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/js/**/*.js',
+      'test/unit/**/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['PhantomJS'],
+
+    singleRun: true,
+
+    reporters: ['progress', 'coverage'],
+
+    preprocessors: { '*.js': ['coverage']},
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine',
+	    'karma-phantomjs-launcher',
+            'karma-junit-reporter'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
